2d9e2b0833ec9df1823883df12c420e23439db76
iedev1 <jurgens@interexcel.co.za>
2015-06-22 13:05:19 +0200

    Update jscript_sagepayfix.js
    Remove jQuery requirement

--- includes/modules/pages/checkout_confirmation/jscript_sagepayfix.js
+++ includes/modules/pages/checkout_confirmation/jscript_sagepayfix.js
@@ -1,7 +1,23 @@
-jQuery(document).ready(function(){
-	/**
-	 * This field is not required for 3rd part forms.
-	 */
-	jQuery('form#checkout_confirmation input[name="securityToken"]').remove();
+if( document.forms.checkout_confirmation ) {
+	// Remove the securityToken field
+	var children = document.forms.checkout_confirmation.childNodes || null,
+		attr = 'name',
+		element;
 
-});
+	if( children ) {
+		for(var i = 0; i < children.length; i++ ) {
+	        element = children[i];
+
+	        if(element.hasAttribute(attr) && element.getAttribute(attr) == 'securityToken') {
+            	element.remove();
+            	break;
+            }
+	    }
+    }
+}
+
+/*
+jQuery(document).ready(function(){
+	// This field is not required for 3rd part forms.
+	jQuery('form#checkout_confirmation input[name="securityToken"]').remove();
+});*/
